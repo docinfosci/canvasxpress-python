d1038ea0cd943e68c08e1f7d128f7b071bcbc638
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-03-17 23:47:36 -0700

    CanvasXpress utility and ancillary

---
+++ canvasxpress/util/template.py
@@ -0,0 +1,19 @@
+def render_from_template(
+        template: str,
+        data: dict
+) -> str:
+    """
+    Updates the template text with the provided data.
+    :param template: The name of the template file
+    :param data: the dict of str values with which to update the template text
+    :returns The adjusted template text
+    """
+    template_text = template
+
+    for key in data.keys():
+        template_text = template_text.replace(
+            f"@{str(key)}@",
+            str(data[key])
+        )
+
+    return template_text
