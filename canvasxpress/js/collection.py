d9fe8406a95eee87110012d75a4c4d1da733672c
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-03-17 23:45:57 -0700

    CanvasXpress Javascript f(x) integration

---
+++ canvasxpress/js/collection.py
@@ -0,0 +1,174 @@
+import collections
+import json
+from copy import copy, deepcopy
+from functools import total_ordering
+from typing import List
+
+from canvasxpress.data.convert import CXJavascriptConvertable
+from canvasxpress.js.function import CXEvent
+
+
+@total_ordering
+class CXEvents(CXJavascriptConvertable):
+    """
+    CXEvents represents a set of CXEvent objects and can render them properly
+    for inclusion with a CanvasXpress object.
+    """
+
+    __events: List[CXEvent] = list()
+    """
+    The react objects tracked by this instance.
+    """
+
+    @property
+    def events(self) -> List[CXEvent]:
+        """
+        A non-associated list of the CXEvents associated with this object.
+        """
+        return copy(self.__events)
+
+    def has(
+            self,
+            event: CXEvent
+    ):
+        """
+        Indicates if the react is a member.
+        :param event: The object to consider
+        """
+        return event in self.events
+
+    def add(
+            self,
+            event: CXEvent,
+            unique: bool = True
+    ) -> None:
+        """
+        Adds the specified react.  If the react must be unique then an Error
+        is raised if an react is already presenbt with the same ID.
+        """
+        if not event:
+            raise TypeError("react cannot be None.")
+
+        if not isinstance(event, CXEvent):
+            raise TypeError("react must be of type CXEvent.")
+
+        if unique and self.has(event):
+            raise ValueError(
+                f"A CXEvent of ID '{event.id}' is already part "
+                f"of this CXEvent object."
+            )
+
+        self.__events.append(event)
+
+    def remove(
+            self,
+            event: CXEvent
+    ) -> bool:
+        """
+        Removes the specified object from the list.
+        :param event: The CXEvent object to remove from the list if it is
+            already included.
+        :returns: True if the react was removed.  False indicates that the
+            object was not a member.
+        """
+        event_removed = False
+        for associated_event in self.__events:
+            if associated_event == event:
+                self.__events.remove(event)
+                event_removed = True
+
+        return event_removed
+
+    def render_to_dict(self) -> dict:
+        """
+        Provides a dict with each js properly formatted as JS within.
+        """
+
+        events = dict()
+        for event in self.events:
+            events[event.id] = event.render_to_js()
+
+        return events
+
+    def render_to_js(self) -> str:
+        """
+        Converts the object into HTML5 complant script.
+        """
+        events = dict()
+        for event in self.events:
+            events[event.id] = f"js_{event.id}"
+
+        html = json.dumps(
+            events,
+            indent=8
+        )
+        for event in self.events:
+            html = html.replace(
+                f'"js_{event.id}"',
+                event.render_to_js()
+            )
+
+        return html
+
+    def __init__(self, *events):
+        """
+        Establishes a new CXEvents object.
+        :param events: A list of CXEvents to associate.
+        """
+        self.__events = list()
+        if events:
+            for event in events:
+                self.add(event)
+
+    def __copy__(self):
+        return CXEvents(
+            *self.events
+        )
+
+    def __deepcopy__(
+            self,
+            memo
+    ):
+        return CXEvents(
+            *([deepcopy(event) for event in self.events])
+        )
+
+    def __lt__(
+            self,
+            other: 'CXEvents'
+    ):
+        if not object:
+            return False
+
+        if type(other) is not CXEvents:
+            return False
+
+        else:
+            return collections.Counter(self.events) < \
+                   collections.Counter(other.events)
+
+    def __eq__(
+            self,
+            other: 'CXEvents'
+    ):
+        if not object:
+            return False
+
+        if type(other) is not CXEvents:
+            return False
+
+        else:
+            return collections.Counter(self.events) == \
+                   collections.Counter(other.events)
+
+    def __str__(self) -> str:
+        return json.dumps(
+            self.render_to_dict()
+        )
+
+    def __repr__(self) -> str:
+        event_rep_list = ", ".join([repr(event) for event in self.events])
+        rep_candidate = f'CXEvents(' \
+                        f'{event_rep_list}' \
+                        f')'
+        return rep_candidate
---
+++ canvasxpress/js/function.py
@@ -0,0 +1,181 @@
+import json
+from copy import copy, deepcopy
+from functools import total_ordering
+
+from canvasxpress.data.convert import CXJavascriptConvertable
+from canvasxpress.util.template import render_from_template
+
+CX_EVENT_TEMPLATE = "function(o, e, t){@cx_js_script@}"
+
+
+@total_ordering
+class CXEvent(CXJavascriptConvertable):
+    """
+    CXEvent represents a Javascript script that can be associated with a
+    CanvasXpress object.
+    """
+
+    __id: str = ""
+    """
+    The triggering JS react, such as mousemove.
+    """
+
+    __script: str = ""
+    """
+    The script to be placed inside of the CanvasXpress react function, which
+    takes the general form:
+    
+    function(o, e, t) {
+        // script logic goes here
+    };
+    """
+
+    @property
+    def id(self) -> str:
+        """
+        Provides access to the react ID.
+        :returns: The ID as a string.
+        """
+        return self.__id
+
+    @id.setter
+    def id(
+            self,
+            value: str
+    ) -> None:
+        """
+        Sets the react ID, which is a keyword recognized by CanvasXpress.
+        :param value: The ID, which must be a string compliant object.
+        """
+        if value == None:
+            raise TypeError("value cannot be None")
+        else:
+            self.__id = str(value)
+
+    @property
+    def script(self) -> str:
+        """
+        Provides access to the react script.
+        :returns: The script as a string.
+        """
+        return self.__script
+
+    @script.setter
+    def script(
+            self,
+            value: str
+    ) -> None:
+        """
+        Sets the react script, which is logic that goes inside of the react
+        function.  Functions take the form:
+
+        function(o, e, t) {
+            // script logic goes here
+        };
+
+        The script can be assumed to have access to all DOM elements as proper,
+        and it will be provided the parameters o, e, and t.  Read more about
+        react functions on the CanvasXpress.org site.
+
+        :param value: The ID, which must be a UTF-8 string compliant object.
+        """
+        if value == None:
+            raise TypeError("value cannot be None")
+        else:
+            self.__script = str(value)
+
+    def render_to_js(self) -> str:
+        """
+        Converts the object into HTML5 complant script.
+        """
+        cx_js = self.script
+        cx_js_func = render_from_template(
+            CX_EVENT_TEMPLATE,
+            {
+                'cx_js_script': cx_js,
+            }
+        )
+        return cx_js_func
+
+    def __init__(
+            self,
+            id: str = "",
+            script: str = ""
+    ):
+        """
+        Provides a new CXEvent object.
+        :param id: The ID of the react, such as mousemove.
+        :param script: The script logic for the react.
+        """
+        self.id = id
+        self.script = script
+
+    def __copy__(self):
+        return CXEvent(
+            id=copy(self.id),
+            script=copy(self.script)
+        )
+
+    def __deepcopy__(
+            self,
+            memo
+    ):
+        return CXEvent(
+            id=deepcopy(self.id),
+            script=deepcopy(self.script)
+        )
+
+    def __hash__(self):
+        return hash(repr(self))
+
+    def __lt__(
+            self,
+            other: 'CXEvent'
+    ):
+        if not object:
+            return False
+
+        if type(other) is not CXEvent:
+            return False
+
+        else:
+            return (self.id < other.id) and (self.script < other.script)
+
+    def __eq__(
+            self,
+            other: 'CXEvent'
+    ):
+        if not object:
+            return False
+
+        if type(other) is not CXEvent:
+            return False
+
+        else:
+            return (self.id == other.id) and (self.script == other.script)
+
+    def __str__(self) -> str:
+        return f'"{self.id}": {self.render_to_js()}'
+
+    @staticmethod
+    def __clean_string(value: str) -> str:
+        candidate = json.dumps(value)[1:-1]
+
+        escape_chars = [
+            ('\\', '\\\\'),
+            ('"', '\\"'),
+            ('\n', '\\n')
+        ]
+        for escape in escape_chars:
+            candidate = candidate.replace(escape[0], escape[1])
+
+        return candidate
+
+    def __repr__(self) -> str:
+        clean_id = CXEvent.__clean_string(self.id)
+        clean_script = CXEvent.__clean_string(self.script)
+        rep_candidate = f'CXEvent(' \
+                        f'id="{clean_id}", ' \
+                        f'script="{clean_script}"' \
+                        f')'
+        return rep_candidate
