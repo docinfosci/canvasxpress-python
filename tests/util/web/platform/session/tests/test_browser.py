ad22330d05d38b6d2c39319dad9bdc3f5cfb760e
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-03-17 23:59:16 -0700

    Test util self-check

---
+++ tests/util/web/platform/session/tests/test_browser.py
@@ -0,0 +1,21 @@
+import allure
+import pytest
+
+from typing import List
+
+# Workaround for PyCharm import bug related to pytest fixtures
+from tests.util.web.platform.browser.generic import ManagedBrowser
+from tests.util.web.platform.session.browser import browsers
+
+_pycharm_pytest_fixture_import_workaround_browsers = browsers
+
+
+@allure.feature('Test Framework Self-Check')
+@pytest.mark.parametrize(
+    "browsers",
+    ["https://www.python.org"],
+    indirect=True
+)
+def test_browser_sessions_for_tests(browsers: List[ManagedBrowser]):
+    for browser in browsers:
+        assert browser.session.title == "Welcome to Python.org"
