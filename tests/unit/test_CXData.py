6771387c0d6bf561cb05a0bd9051d051f2ed11ea
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-03-26 10:47:52 -0700

    Increased unit test coverage of CXData to 100%

---
+++ tests/unit/test_CXData.py
@@ -0,0 +1,44 @@
+from canvasxpress.data.base import CXData
+
+
+class TestableData(CXData):
+    """
+    A generic CXData for testing the abstract base class.
+    """
+
+    __data: dict = None
+
+    def __init__(self, data: dict) -> None:
+        super().__init__(data)
+        if data:
+            self.__data = data
+        else:
+            self.__data = dict()
+
+    def data(self) -> dict:
+        result = super().data
+        if not result:
+            result = self.__data
+        return result
+
+    def render_to_dict(self) -> dict:
+        result = super().render_to_dict()
+        if not result:
+            result = self.__data
+        return result
+
+
+def test_CXData_get_data():
+    sample_data = {
+        "y": {
+            "vars": ["Gene1"],
+            "smps": ["Smp1", "Smp2", "Smp3"],
+            "data": [[10, 35, 88]]
+        }
+    }
+
+    testable_data: TestableData = TestableData(
+        sample_data
+    )
+
+    assert testable_data.data() == sample_data
