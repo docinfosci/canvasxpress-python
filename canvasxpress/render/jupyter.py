16ff7629621a921c7cd2ec6e61a9bac47ecfb832
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-04-09 14:37:31 -0700

    Support refactored attributes

--- canvasxpress/render/jupyter.py
+++ canvasxpress/render/jupyter.py
@@ -47,8 +47,8 @@ class CXNoteBook(CXRenderable):
         display(
             IFrame(
                 temp_filename,
-                self.canvas.canvas_width + 10,
-                self.canvas.canvas_height + 10
+                self.canvas.chart_width + 10,
+                self.canvas.chart_height + 10
             ),
             metadata=dict(isolated=True)
         )
