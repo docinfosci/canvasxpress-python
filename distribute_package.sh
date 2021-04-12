ac9b17aa806a737182b4b99adde36c49d837f6ea
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-04-12 15:41:17 -0700

    Support bdist_wheel; only publish non-dev releases

--- distribute_package.sh
+++ distribute_package.sh
@@ -10,20 +10,31 @@ source ./venv/bin/activate
 rm -rf ./dist
 python ./generate_setup_instructions.py
 python setup.py sdist
-python setup.py bdist
+python setup.py bdist_wheel
 
-# Confirm the installation user account
-echo $TWINE_USERNAME
+# Only publish non-dev editions to PyPI
+GIT_WORKING_BRANCH="$(git branch | grep \* | cut -d ' ' -f2)"
+if [ $GIT_WORKING_BRANCH = "main" ]
+then
+  # Confirm the installation user account
+  echo $TWINE_USERNAME
 
-# Acquire twine and push the package
-pip install twine
-twine upload --verbose -u $TWINE_USERNAME -p $TWINE_PASSWORD --repository-url https://tools-prd.aggregate-genius.com/python-packages/ ./dist/*
-TWINE_EXIT=$?
+  # Acquire twine and push the package
+  pip install twine
+  twine upload --verbose -u $TWINE_USERNAME -p $TWINE_PASSWORD --repository-url https://tools-prd.aggregate-genius.com/python-packages/ ./dist/*
+  TWINE_EXIT=$?
+
+  # Fix for confirming status to the build server
+  if (("$TWINE_EXIT" != 0))
+  then
+      exit 1
+  fi
+
+else
+  # PyPI culture is that dev editions are not installed.
+  echo "Edition is non-production so no installation performed"
+
+fi
 
 # We're finished with the virtual environment
 deactivate
-
-# Fix for confirming status to the build server
-if (("$TWINE_EXIT" != 0)); then
-    exit 1
-fi
