6cf3377dca5574739cc8ac0e11847702c7e37d30
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-04-16 12:18:49 -0700

    Accomodate circleci's older debian images

--- init-webdrivers.sh
+++ init-webdrivers.sh
@@ -21,8 +21,32 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
 
     # Install the Chrome browser and webdriver
     sudo apt-get -yq update; sudo apt-get -yq install --fix-missing chromium-browser
-    which chromium-browser
-    chromium-browser --version
+    CHROMIUM_BROWSER_INSTALL_EXIT=$?
+
+    # If chromium-browser is not available try for older chromium
+    if (("CHROMIUM_BROWSER_INSTALL_EXIT" != 0)); then
+        echo "Cannot install chromium-browser, attempting to install chromium..."
+        sudo apt-get -yq update; sudo apt-get -yq install --fix-missing chromium
+        CHROMIUM_BROWSER_INSTALL_EXIT=$?
+
+        # If chromium is also not available there's no point to continuing
+        if (("CHROMIUM_BROWSER_INSTALL_EXIT" != 0)); then
+            echo "Cannot install chromium, exiting build"
+            exit 1
+        
+        # Otherwise verify the chromium installation
+        else
+            which chromium
+            chromium --version
+
+        fi
+
+    # Otherwise verify the chromium-browser installation
+    else
+        which chromium-browser
+        chromium-browser --version
+
+    fi
 
     sudo apt-get -yq update; sudo apt-get -yq install --fix-missing chromium-chromedriver
     which chromedriver
