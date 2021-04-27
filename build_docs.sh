6321caa623736df25ff726c032cdd08ea96a5b6f
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-04-27 15:25:12 -0700

    Support example images

--- build_docs.sh
+++ build_docs.sh
@@ -3,6 +3,8 @@ rm -rf build docs site mkdocs.yml
 export SITE_DIR="site"
 pydoc-markdown --build --site-dir $SITE_DIR
 cp -R build/docs/content docs
+cp -R readme/examples docs/examples
+cp -R readme/images docs/images
 cp build/docs/mkdocs.yml mkdocs.yml
 sed -i 's/docs_dir: content/docs_dir: docs/' mkdocs.yml
 mkdocs build
