48c75eed869fe7afec371690798c9b6eee66bc8c
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-04-30 09:09:25 -0700

    Documentation updates per team feedback

--- build_docs.sh
+++ build_docs.sh
@@ -1,10 +1,18 @@
 # Document the project
+
+# Clear any existing build artifacts
 rm -rf build docs site mkdocs.yml
+
+# Produce API documentation and mkdocs templates
 export SITE_DIR="site"
 pydoc-markdown --build --site-dir $SITE_DIR
+
+# Move mkdocs templates to expected build locations for readthedocs.io build
 cp -R build/docs/content docs
 cp -R readme/examples docs/examples
 cp -R readme/images docs/images
 cp build/docs/mkdocs.yml mkdocs.yml
 sed -i 's/docs_dir: content/docs_dir: docs/' mkdocs.yml
+
+# For local inspection, also run mkdocs
 mkdocs build
