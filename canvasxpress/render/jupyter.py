527098f8913770ff4c721d4ffdd0393cdeff80d0
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-07-06 15:43:56 -0700

    Always append .html to output file if extension is not provided

--- canvasxpress/render/jupyter.py
+++ canvasxpress/render/jupyter.py
@@ -184,9 +184,14 @@ class CXNoteBook(CXRenderable):
                 f"cx_{str(uuid.uuid4())}.html"
             )
         )
+
         file_path = Path(file_path_candidate)
         if file_path.is_dir():
             file_path = file_path.joinpath(f"cx_{str(uuid.uuid4())}.html")
+        else:
+            if not file_path_candidate.lower().endswith('.html'):
+                file_path_candidate = file_path_candidate + ".html"
+                file_path = Path(file_path_candidate)
 
         try:
             with open(str(file_path), "w") as render_file:
