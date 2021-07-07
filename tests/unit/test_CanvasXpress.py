4b6b20bbb384b053f932d6d22a0a189182721eab
Dr. Todd C. Brett <todd@aggregate-genius.com>
2021-07-07 14:54:47 -0700

    Update unit tests

--- tests/unit/test_CanvasXpress.py
+++ tests/unit/test_CanvasXpress.py
@@ -21,7 +21,7 @@ def test_CanvasXpress_init():
     subject: CanvasXpress = CanvasXpress(
         render_to="this_is_a_test"
     )
-    assert subject.render_to == "this_is_a_test"
+    assert subject.render_to == "thisisatest"
 
     raw_sample = {'a': [1]}
     data_sample = CXDictData(raw_sample)
@@ -60,7 +60,7 @@ def test_CanvasXpress_init():
 def test_CanvasXpress_render_to():
     subject: CanvasXpress = CanvasXpress()
     subject.render_to = "this_is_a_test"
-    assert subject.render_to == "this_is_a_test"
+    assert subject.render_to == "thisisatest"
 
     with pytest.raises(ValueError):
         subject: CanvasXpress = CanvasXpress()
@@ -69,10 +69,6 @@ def test_CanvasXpress_render_to():
     with pytest.raises(TypeError):
         subject: CanvasXpress = CanvasXpress()
         subject.render_to = 1
-
-    with pytest.raises(ValueError):
-        subject: CanvasXpress = CanvasXpress()
-        subject.render_to = "%*&*("
 
 
 def test_CanvasXpress_chart_width():
