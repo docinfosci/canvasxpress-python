d9ef0a2b06a61859afc0676e57e2afd027cd2bf8
docinfosci <todd@aggregate-genius.com>
2021-12-10 13:04:50 -0800

    Remove firefox from tests

--- tests/util/web/platform/browser/tests/test_firefox.py
+++
@@ -1,9 +0,0 @@
-import allure
-
-from tests.util.web.platform.browser.firefox import FireFoxManagedBrowser
-
-
-@allure.feature('Test Framework Self-Check')
-def test_firefox_browser():
-    with FireFoxManagedBrowser("https://www.python.org") as browser:
-        assert browser.session.title == "Welcome to Python.org"
