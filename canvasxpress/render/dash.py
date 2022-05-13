75e729681a80eca860c30e3c89df282c7782e23e
Dr. Todd C. Brett <todd@aggregate-genius.com>
2022-05-13 16:06:43 -0700

    Fix for Dash list conversion in afterRender support (#94)

--- canvasxpress/render/dash.py
+++ canvasxpress/render/dash.py
@@ -47,7 +47,9 @@ class CXElementFactory(CXRenderFactory):
                 {**element_parts["config"], **element_parts["otherParams"]}
             ),
             events=element_parts["events"],
-            after_render=element_parts["afterRender"],
+            after_render=json.dumps(
+                element_parts["afterRender"]
+            ),
             width=str(element_parts["width"]),
             height=str(element_parts["height"]),
         )
