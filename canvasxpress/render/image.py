9eb68802f60ed7c74ce524c0217ae795e799627c
docinfosci <todd@aggregate-genius.com>
2022-12-14 10:56:23 -0800

    Revised node_modules, npx, and return logic

--- canvasxpress/render/image.py
+++ canvasxpress/render/image.py
@@ -1,8 +1,9 @@
 import os
-import base64
 import subprocess
+import json
 from typing import Any, Union, List
-from tempfile import gettempdir, NamedTemporaryFile
+from tempfile import gettempdir
+from pathlib import Path
 
 from canvasxpress.canvas import CanvasXpress
 from canvasxpress.render.base import CXRenderable
@@ -10,6 +11,43 @@ from canvasxpress.render.json import CXJSON
 
 PNG_IMAGE: str = "png"
 SVG_IMAGE: str = "svg"
+
+
+def install_cx_in_nodejs() -> None:
+    availability_status = subprocess.run(
+        ["npm", "ls", "canvasxpress-cli"], capture_output=True
+    )
+    if availability_status.returncode != 0:
+        installation_status = subprocess.run(
+            ["npm", "install", "canvasxpress-cli", "--save"], capture_output=True
+        )
+        if installation_status.returncode != 0:
+            raise RuntimeError(
+                f"Image rendering is unavailable. "
+                f"Cannot install canvasxpress NodeJS package: {installation_status.stderr.decode('utf-8')}"
+            )
+
+
+def nodejs_modules_path() -> Path:
+    """
+    Returns the path to the NodeJS' modules installation.
+    :returns: `str`
+        The path for the NodeJS' modules installation.
+    """
+    install_cx_in_nodejs()
+    search_path = Path(os.path.abspath(__file__)).parent
+    while search_path != "":
+        test_path = Path(search_path, "node_modules")
+        if test_path.exists():
+            return test_path
+        else:
+            if search_path == search_path.root:
+                raise RuntimeError("node_modules is not available.")
+            else:
+                search_path = search_path.parent
+
+
+CX_NODEJS_PATH: Path = nodejs_modules_path() / "canvasxpress-cli/bin/canvasxpress"
 
 
 class CXImage(CXRenderable):
@@ -22,29 +60,13 @@ class CXImage(CXRenderable):
     def __init__(self, *cx: Union[List[CanvasXpress], CanvasXpress, None]):
         """
         Initializes a new `CXNoteBook` object.
-        :praram cx: `Union[List[CanvasXpress], CanvasXpress, None], ...`
+        :param cx: `Union[List[CanvasXpress], CanvasXpress, None], ...`
             The `CanvasXpress` object(s) to be tracked.  See the `canvas`
             property, except that on initialization cx can be `None`.
             Multiple CanvasXpress objects are supported provided that
             they have distinct `render_to` targets.
         """
         super().__init__(*cx)
-        CXImage.install_cx_in_nodejs()
-
-    @classmethod
-    def install_cx_in_nodejs(cls):
-        availability_status = subprocess.run(
-            ["npm", "ls", "canvasxpress-cli"], capture_output=True
-        )
-        if availability_status.returncode != 0:
-            installation_status = subprocess.run(
-                ["npm", "install", "canvasxpress-cli", "--save"], capture_output=True
-            )
-            if installation_status.returncode != 0:
-                raise RuntimeError(
-                    f"Image rendering is unavailable. "
-                    f"Cannot install canvasxpress NodeJS package: {installation_status.stderr.decode('utf-8')}"
-                )
 
     def render(self, **kwargs: Any) -> list:
         """
@@ -69,41 +91,45 @@ class CXImage(CXRenderable):
                     "format must be one of PNG_IMAGE or SVG_IMAGE, or a list of each."
                 )
 
-            for json in reproducible_jsons:
-                work_dir = "." # gettempdir()
-                work_json_path = os.path.join(work_dir, "cx_data.json")
-                work_image_path = os.path.join(work_dir, f"cx_image.{image_format}")
+            for reproducible_json in reproducible_jsons:
+                work_dir = Path(gettempdir()) / "org.canvasxpress.render"
+                work_dir.mkdir(exist_ok=True)
+                work_json_path = Path(work_dir, "cx_data.json")
+                work_image_path = work_dir
 
                 with open(work_json_path, "w") as json_temp_file:
-                    json_temp_file.write(json)
+                    json_temp_file.write(reproducible_json)
 
                 result = subprocess.run(
                     [
-                        "npx",
-                        "canvasxpress",
-                        image_format,
-                        "-i",
-                        work_json_path,
-                        "-o",
-                        work_image_path,
+                        f"{CX_NODEJS_PATH} {image_format} -i {work_json_path} -o {work_image_path}",
                     ],
                     shell=True,
                     capture_output=True,
                 )
                 if result.returncode == 0:
 
-                    with open(work_image_path, "rb") as image_file:
-                        image = base64.b64encode(image_file.read())
+                    image_file_options = list(
+                        work_image_path.glob(f"**/*.{image_format}")
+                    )
+                    image_file_path = image_file_options[0]
+
+                    with open(Path(image_file_path), "rb") as image_file:
+                        image: bytes = image_file.read()
+                    image_file_path.unlink()
 
                     rendered_images.append(
                         {
-                            "id": json.loads(json).get("render_to", "anonymous"),
-                            "image_base64": image,
+                            "id": json.loads(reproducible_json).get(
+                                "renderTo", "anonymous"
+                            ),
+                            "image": {
+                                "binary": image,
+                                "format": image_format,
+                            },
                         }
                     )
 
-                    os.remove(work_image_path)
+                work_json_path.unlink()
 
-                os.remove(work_json_path)
-
-        return reproducible_jsons
+        return rendered_images
