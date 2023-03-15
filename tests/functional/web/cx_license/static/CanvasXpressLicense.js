1fa31f8ef2cff8072cd52cd542dd4d3198796771
docinfosci <todd@aggregate-genius.com>
2023-03-15 15:52:24 -0700

    Web tests no longer valid, to be replaced.

--- tests/functional/web/cx_license/static/CanvasXpressLicense.js
+++
@@ -1,5 +0,0 @@
-// Create a js file named 'CanvasXpressLicense.js' with the content
-// below and include it as a source before canvasXpress.min.js
-// This license is valid until YYYY-MM-DD.
-
-window.cX=LICENSE_DATA_WOULD_GO_HERE";
