4c08eb891847144674a8e030606a3ef900c14401
docinfosci <todd@aggregate-genius.com>
2023-07-24 11:58:22 -0700

    More attempts to resolve CX usage of crypto

--- streamlit/cxstreamlit/frontend/webpack.config.js
+++ streamlit/cxstreamlit/frontend/webpack.config.js
@@ -1,10 +1,24 @@
-module.exports = function (webpackEnv) {
-    return {
-        resolve: {
-            fallback: {
-                crypto: require.resolve("crypto-browserify"),
-                stream: require.resolve("stream-browserify"),
-            }
-        }
-    }
+/* config-overrides.js */
+const webpack = require('webpack');
+module.exports = function override(config, env) {
+    //do stuff with the webpack config...
+
+    config.resolve.fallback = {
+        url: require.resolve('url'),
+        assert: require.resolve('assert'),
+        crypto: require.resolve('crypto-browserify'),
+        http: require.resolve('stream-http'),
+        https: require.resolve('https-browserify'),
+        os: require.resolve('os-browserify/browser'),
+        buffer: require.resolve('buffer'),
+        stream: require.resolve('stream-browserify'),
+    };
+    config.plugins.push(
+        new webpack.ProvidePlugin({
+                                      process: 'process/browser',
+                                      Buffer: ['buffer', 'Buffer'],
+                                  }),
+    );
+
+    return config;
 }
