64585e9f136c0c0c3c4f85b5bd3109c087d6556f
docinfosci <todd@aggregate-genius.com>
2023-07-26 09:26:11 -0700

    Reduce to display function

--- canvasxpress/render/streamlit.py
+++ canvasxpress/render/streamlit.py
@@ -3,42 +3,31 @@ This module provides an integration solution for the CanvasXpress class and the 
 rich integration with Streamlit's framework.
 """
 import json
-from typing import List, Union, Any
+from typing import Union
 
 from canvasxpress.canvas import CanvasXpress
-from canvasxpress.render.base import CXRenderFactory
 from cxstreamlit import CXStreamlit
 
 
-class CXElementFactory(CXRenderFactory):
+def display(cx: CanvasXpress) -> Union[object, None]:
     """
-    CXStreamlitFactory converts CanvasXpress objects into PlotlyStreamlit elements that will
-    render CanvasXpress charts into the Streamlit UI.
+    Converts the provided CanvasXpress object into a CXStreamlit object that is
+    primed by the configuration represented by the CanvasXpress object.
+    :param cx: `CanvasXpress` The CanvasXpress object to render.  An exception is
+        raised if cx is `None`.
+    :returns: `CXStreamlit` A CXStreamlit with the configuration as represented
+        by cx.  'None' if cx is invalid.
     """
 
-    def __init__(self, *cx: Union[List[CanvasXpress], CanvasXpress, None]):
-        """
-        Initializes a new `CXStreamlitRenderFactory` object.
-        :param cx: `Union[List[CanvasXpress], CanvasXpress, None], ...`
-            The `CanvasXpress` object(s) to be tracked.  See the `canvas`
-            property, except that on initialization cx can be `None`.
-            Multiple CanvasXpress objects are supported provided that
-            they have distinct `render_to` targets.
-        """
-        super().__init__(*cx)
+    if cx is None:
+        return None
 
-    @classmethod
-    def render(cls, cx: CanvasXpress) -> CXStreamlit:
-        """
-        Converts the provided CanvasXpress object into a CXStreamlit object that is
-        primed by the configuration represented by the CanvasXpress object.
-        :param cx: `CanvasXpress` The CanvasXpress object to render.  An exception is
-            raised if cx is `None`.
-        :returns: `CXStreamlit` A CXStreamlit with the configuration as represented
-            by cx.
-        """
+    if not isinstance(cx, CanvasXpress):
+        return None
+
+    else:
         element_parts = cx.prepare_html_element_parts()
-        streamlit_element = CXStreamlit(
+        streamlit_element: object = CXStreamlit(
             id=element_parts["renderTo"],
             data=element_parts["data"]
             if isinstance(element_parts["data"], str)
@@ -54,19 +43,3 @@ class CXElementFactory(CXRenderFactory):
         )
 
         return streamlit_element
-
-    def render_all(self, **kwargs: Any) -> List[CXStreamlit]:
-        """
-        Provides a list of objects that can be used by the target domain or container
-        to create CanvasXpress illustrations or instantiations.
-        Not implemented.
-        :param kwargs: `Any`
-            Parameters specific to implementations are supported.  The essential
-            create_element call should work with no extra parameters, and with
-            parameters that do not apply to the implementation.
-        """
-        if isinstance(self.canvas, CanvasXpress):
-            return [CXElementFactory.render(self.canvas)]
-
-        else:
-            return [CXElementFactory.render(cx) for cx in self.canvas]
