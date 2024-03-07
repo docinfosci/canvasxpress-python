bb0fdf843885c047b61bd08cf5aeee044f30545d
docinfosci <todd@aggregate-genius.com>
2024-03-07 15:12:19 -0800

    CircleCI refinement for Webpack > 5

--- streamlit/cxstreamlit/frontend/webpack.config.js
+++ streamlit/cxstreamlit/frontend/webpack.config.js
@@ -12,7 +12,7 @@ module.exports = function override(config, env) {
         os: require.resolve('os-browserify/browser'),
         buffer: require.resolve('buffer'),
         stream: require.resolve('stream-browserify'),
-        vm: require.resolve("vm-browserify")
+        vm: false
     };
     config.plugins.push(
         new webpack.ProvidePlugin({
