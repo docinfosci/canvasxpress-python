65c114c39f5c5c22f746f460850f61ab472eb581
docinfosci <todd@aggregate-genius.com>
2024-07-24 11:52:30 -0700

    Fix for Shiny Python 1.0

--- canvasxpress/render/shiny.py
+++ canvasxpress/render/shiny.py
@@ -6,44 +6,23 @@ from shiny import ui
 from canvasxpress.canvas import CanvasXpress
 
 _cx_default_css_url = "https://www.canvasxpress.org/dist/canvasXpress.css"
-_cx_versioned_css_url = (
-    "https://cdnjs.cloudflare.com/ajax/libs/canvasXpress/@cx_version@/canvasXpress.css"
-)
+_cx_versioned_css_url = "https://cdnjs.cloudflare.com/ajax/libs/canvasXpress/@cx_version@/canvasXpress.css"
+
 _cx_default_js_url = "https://www.canvasxpress.org/dist/canvasXpress.min.js"
 _cx_versioned_js_url = "https://cdnjs.cloudflare.com/ajax/libs/canvasXpress/@cx_version@/canvasXpress.min.js"
-_cx_intermixed_header = """
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <link 
-                href='@css_url@' 
-                rel='stylesheet' 
-                type='text/css'
-        />
-        <script 
-                src='@js_url@' 
-                type='text/javascript'>
-        </script>
-    </head>
-    <body></body>
-</html>
-"""
+
 _cx_js_intermixed_template = """
 <script type="text/javascript">
     @code@
 </script>
 """
+
 _cx_html_intermixed_template = """
-<html>
-    <head>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-        @canvasxpress_license@
-        @canvase@
-        @js_functions@
-    </body>
-</html>
+<div>
+    @canvasxpress_license@
+    @canvase@
+    @js_functions@
+</div>
 """
 
 
@@ -89,35 +68,9 @@ class CXShinyWidget(object):
         # Provide the taglist.
         components = [
             ui.div(
-                ui.HTML(
-                    """
-                    <script type="text/javascript">
-                    // CSS inclusion
-                    css_dom_rsrc_id = "CXDashElementIncludeCSS";
-                    if (!document.getElementById(css_dom_rsrc_id)) {
-                        var head = document.getElementsByTagName("head")[0];
-                        var s = document.createElement("link");
-                        s.type = "text/css";
-                        s.href = "@css_url@";
-                        s.id = css_dom_rsrc_id;
-                        head.appendChild(s);
-                    }
-                    // JS inclusion
-                    js_dom_rsrc_id = "CXDashElementIncludeScript";
-                    if (!document.getElementById(js_dom_rsrc_id)) {
-                        var head = document.getElementsByTagName("head")[0];
-                        var s = document.createElement("script");
-                        s.type = "text/javascript";
-                        s.src = "@js_url@";
-                        s.id = js_dom_rsrc_id;
-                        head.appendChild(s);
-                    }
-                    </script>
-                    """.replace(
-                        "@css_url@", css_url
-                    ).replace(
-                        "@js_url@", js_url
-                    )
+                ui.head_content(
+                    ui.tags.link(href=css_url, rel="stylesheet"),
+                    ui.tags.script(src=js_url),
                 ),
                 ui.HTML(
                     _cx_html_intermixed_template.replace(
@@ -155,26 +108,6 @@ class CXShinyWidget(object):
         except:
             robjects = None
 
-        intermixed_header = """
-        <link 
-                href='@css_url@' 
-                rel='stylesheet' 
-                type='text/css'
-        />
-        <script 
-                src='@js_url@' 
-                type='text/javascript'>
-        </script>
-        """
-
-        html_intermixed_template = """
-        <div>
-            @canvasxpress_license@
-            @canvase@
-            @js_functions@
-        </div>
-        """
-
         # Get the header assets.
         css_url = _cx_default_css_url
         js_url = _cx_default_js_url
@@ -186,19 +119,18 @@ class CXShinyWidget(object):
                 "@cx_version@", CanvasXpress.cdn_edition()
             )
 
-        header_html_text = intermixed_header.replace("@css_url@", css_url).replace(
-            "@js_url@", js_url
-        )
-
         # Get the HTML and JS assets.
         html_parts: dict = self._canvas.render_to_html_parts()
 
         # Provide the taglist.
         components = [
             ui.div(
-                ui.HTML(header_html_text),
+                ui.head_content(
+                    ui.tags.link(href=css_url, rel="stylesheet"),
+                    ui.tags.script(src=js_url),
+                ),
                 ui.HTML(
-                    html_intermixed_template.replace(
+                    _cx_html_intermixed_template.replace(
                         "@canvasxpress_license@",
                         html_parts.get("cx_license", ""),
                     )
