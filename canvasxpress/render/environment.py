a38883200a63f71818b37ec17f349ea21329108d
docinfosci <todd@aggregate-genius.com>
2026-09-02 16:21:20 -0700

    Modernize docstrings in canvasxpress/render/environment.py
    Convert all module-level function docstrings to Google Python Style Guide
    format. Updated is_rstudio_active(), is_shiny_available(), is_dash_available(),
    is_ipython_available(), is_streamlit_available(), get_target_context() with
    Args:/Returns: sections. Fixed :returns" typos to Returns: sections.

--- canvasxpress/render/environment.py
+++ canvasxpress/render/environment.py
@@ -5,8 +5,8 @@ import os
 from os import environ
 
 CANVASXPRESS_TARGET_CONTEXT: str = "CANVASXPRESS_TARGET_CONTEXT"
-"""
-The ENV variable that can be set to indicate which context should be targetted regardless of what is detected.
+"""The ENV variable that can be set to indicate which context should be targetted regardless of what is detected.
+
 Valid options are:
 
 - shiny
@@ -37,7 +37,9 @@ VALID_CONTEXTS: list = [
 def is_rstudio_active() -> bool:
     """
     Indicates if RStudio is active at the time of function call.
-    :returns" `bool` `True` if the RStudio IDE is running.
+
+    Returns:
+        True if the RStudio IDE is running, False otherwise.
     """
     try:
         import shiny
@@ -51,7 +53,9 @@ def is_rstudio_active() -> bool:
 def is_shiny_available() -> bool:
     """
     Indicates if shiny is available at the time of function call.
-    :returns" `bool` `True` if shiny is available.
+
+    Returns:
+        True if shiny is available, False otherwise.
     """
     try:
         import shiny
@@ -65,7 +69,9 @@ def is_shiny_available() -> bool:
 def is_dash_available() -> bool:
     """
     Indicates if dash is available at the time of function call.
-    :returns" `bool` `True` if dash is available.
+
+    Returns:
+        True if dash is available, False otherwise.
     """
     try:
         import dash
@@ -82,7 +88,10 @@ def is_dash_available() -> bool:
 def is_ipython_available() -> bool:
     """
     Indicates if IPython is available at the time of function call.
-    :returns" `bool` `True` if IPython is available.
+
+    Returns:
+        True if IPython is available with an active shell (Jupyter notebook or qtconsole),
+        False otherwise.
     """
     try:
         from IPython import get_ipython
@@ -115,7 +124,9 @@ def is_ipython_available() -> bool:
 def is_streamlit_available() -> bool:
     """
     Indicates if streamlit is available at the time of function call.
-    :returns" `bool` `True` if streamlit is available.
+
+    Returns:
+        True if streamlit is available, False otherwise.
     """
     try:
         import streamlit
@@ -129,7 +140,12 @@ def is_streamlit_available() -> bool:
 def get_target_context() -> str:
     """
     Indicates the runtime context that should be targetted for illustrating charts.
-    :returns" `str` One of the options for VALID_CONTEXTS.
+
+    The context is determined by checking the environment variable CANVASXPRESS_TARGET_CONTEXT,
+    then checking in order: Jupyter/IPython, Dash, RStudio, Shiny, Streamlit.
+
+    Returns:
+        One of the options from VALID_CONTEXTS, or CONTEXT_UNKNOWN if no context is detected.
     """
     target = environ.get(CANVASXPRESS_TARGET_CONTEXT)
 
