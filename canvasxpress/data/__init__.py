9e805f0a580c922ecf9500c26e6d3dda0bec5184
docinfosci <todd@aggregate-genius.com>
2026-09-02 16:19:12 -0700

    Modernize docstrings in canvasxpress/data/__init__.py
    Convert module docstring to Google Python Style Guide format with proper
    inline style.

--- canvasxpress/data/__init__.py
+++ canvasxpress/data/__init__.py
@@ -1,5 +1,4 @@
-"""
-The data package provides functionality for integrating different kinds of data
+"""The data package provides functionality for integrating different kinds of data
 structures and sources into a CanvasXpress object.  A balance is provided
 between convenience functionality and reasonable external preparation, such as
 what can be performed via `pandas`.
